f3a8213d485db1c6cc9672dfe083d1f26f2e7a47
Beth Douglas <bethDdouglas@gmail.com>
2018-01-18 14:05:08 -0500

    GAME OVER

---
+++ PacMan-Challenge/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacMan_Challenge
+{
+    class Point
+    {
+        public enum Points
+        {
+            dot = 10,
+            cherry = 100,
+            strawberry = 300,
+            orange = 500,
+            apple = 700,
+            melon = 1000,
+            galaxian = 2000,
+            bell = 3000,
+            key = 5000,
+            vunurableghost = 200, //doubled (++ equivalant of x2?)
+        }
+
+
+    }
+}
--- PacMan-Challenge/Program.cs
+++ PacMan-Challenge/Program.cs
@@ -10,6 +10,16 @@ namespace PacMan_Challenge
     {
         static void Main(string[] args)
         {
+            //Console.Read csv
+            //to lowercase
+            //while reading csv
+            //use enum points? add to total points (start at 5000)
+            //if "invinsible ghost" remove life (start at 3)
+            //if total points >= 10,000, add life
+            //when life =0 cw GAME OVER
+            //or cw total points and lives
+
+            Console.WriteLine("GAME OVER (after Key, InvinsibleGhost)");
         }
     }
 }
